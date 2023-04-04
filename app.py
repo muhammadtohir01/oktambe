373a7ba5fed35759a0688dfc58091195b653a7be
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2023-04-04 14:46:55 +0500

    hammasi

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, request
+from telegram import Bot, Update
+from telegram.ext import Dispatcher, CommandHandler, MessageHandler, CallbackQueryHandler, Filters
+
+# import callback functions
+from main import (
+    start,
+    query,
+)
+
+app = Flask(__name__)
+
+# bot
+TOKEN = os.environ['TOKEN']
+bot = Bot(token=TOKEN)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def main():
+    if request.method == 'GET':
+        return {'status': 200}
+
+    elif request.method == 'POST':
+        # get data from request
+        data: dict = request.get_json(force=True)
+
+        # convert data to Update obj
+        update: Update = Update.de_json(data, bot)
+
+        # Dispatcher
+        dp: Dispatcher = Dispatcher(bot, None, workers=0)
+
+        # handlers
+        dp.add_handler(CommandHandler('start',start))
+    
+        dp.add_handler(CallbackQueryHandler(query))
+
+        # process update
+        dp.process_update(update=update)
+        return {'status': 400}
+    print(bot.setWebhook('https://oktambek01.pythonanywhere.com/'))
